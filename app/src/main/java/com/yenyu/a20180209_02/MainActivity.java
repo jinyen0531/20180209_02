978d7f9f436aabee7d89749e2a88980b8db75802
yen <jinyen0531@gmail.com>
2018-02-09 11:19:15 +0800

    Initial commit

---
+++ app/src/main/java/com/yenyu/a20180209_02/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yenyu.a20180209_02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/yenyu/a20180209_02/MywidgetProivder.java
@@ -0,0 +1,10 @@
+package com.yenyu.a20180209_02;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by Student on 2018/2/9.
+ */
+
+public class MywidgetProivder extends AppWidgetProvider {
+}
